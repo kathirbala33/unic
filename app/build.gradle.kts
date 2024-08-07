e49e7db35af841b9667e2a409e5fed17ecd3ab88
kathiresan.b <kathiresan.b@atyati.com>
2024-08-07 22:26:38 +0530

    1.7 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yes.unic"
         minSdk = 21
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.6"
+        versionCode = 7
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //password - compress
